7c50ee5c3f3711d6ca6e95059e719bda9a551c7f
root <konjou@gmail.com>
2019-03-14 01:47:09 +0100

    everything

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactTable from "react-table";
 import './App.css';
+import 'react-table/react-table.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      data: null,
+      columns: [],
+    };
+  }
+
+  componentDidMount(){
+
+    const data2 = {"chain":[{"index":1,"previous_hash":1,"proof":100,"timestamp":1552397414.8457828,"transactions":[]},{"index":2,"previous_hash":"dd1f146001496034880422bed8764f9a6f0bb6e904609d72a37751854cbc4ca0","proof":35293,"timestamp":1552398174.6053038,"transactions":[{"amount":"3","recipient":"2","sender":"1"},{"amount":1,"recipient":"18d2267430be4a7fa95db37348ad2f2a","sender":"0"}]}],"length":2};
+    const columns = [{Header:"Block",accessor:'block'},{Header:'Sender',accessor: 'sender'},{Header: 'Recipient',accessor: 'recipient'},{Header:'Amount',accessor:'amount'}];
+
+    fetch('/chain')
+    .then(results => results.json())
+    .then(data => 
+	{
+	var chain = []
+	for (let i = 0; i < data.chain.length; i++){
+	  if( data.chain[i].transactions != []){
+	  for (let j = 0; j < data.chain.transactions.length; j++){
+		chain.push({
+		'block':i,
+		'sender':data.chain[i].transactions[j].sender, 
+		'recipient':data.chain[i].transactions[j].recipient,
+		'amount':data.chain[i].transactions[j].amount,
+		});
+	    }
+	  }
+	  else {chain.push({
+		'block':i, 
+		'sender':'',
+		'recipient':'',
+		'amount':'',
+		});
+	  }
+	}
+
+        }
+	this.setState({data:chain, columns:columns, isLoading:false})
+	})
+    .catch(error => this.setState({isLoading: false}));
+  }
+
   render() {
+
+	const { isLoading, data, columns } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+	<React.Fragment>
+	{!isLoading ?<div>
+		<h2>Nanobit Blockchain Explorer</h2>
+           <ReactTable
+             data = {data}
+             columns = {columns}
+             defaultPageSize = {3}
+             pageSizeOptions = {[3,6]}
+           /> </div>
+	: <h3>Loading...</h3>}
+	</React.Fragment>
     );
   }
 }
